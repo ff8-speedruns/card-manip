8f530a94e8ff0bb8ac9f78a7be18b3bbd7261e2b
brofar <behfar@otafest.com>
2022-07-04 13:10:30 -0600

    Make RNG flexible

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import settings from './json/test.json' assert {type: 'json'};
 
-let baseRng = 0;
+let baseRng = {};
 
 settings.forEach((node) => {
     // Begin building each section
@@ -12,18 +12,24 @@ settings.forEach((node) => {
     container.appendChild(title);
 
     // Base RNG
-    baseRng += node.baseValue;
+    if (!node.hasOwnProperty('baseCountPage')) {
+        node.baseCountPage = node.label;
+        baseRng[node.label] = parseInt(node.baseValue);
+    }
 
     // Options
     node.categories.forEach((category) => {
         let categoryLabel = document.createElement('h5');
         categoryLabel.innerHTML = category.label;
+        console.warn(category.label);
         container.appendChild(categoryLabel);
 
         // The element
         category.subCategories.forEach((subCategory) => {
+            if (subCategory.type.length == 0)
+                return;
             let times = subCategory.times || 1;
-            
+
             let holder = document.createElement('div');
 
             if (times > 1) {
@@ -33,50 +39,51 @@ settings.forEach((node) => {
             // Loop for however many times this subcategory is repeated
             for (let i = 0; i < times; i++) {
                 // Create HTML element
-                let element = CreateElement(subCategory.type, subCategory.value, subCategory.label);
+                let element = CreateElement(subCategory.type, subCategory.value, node.baseCountPage, subCategory.label);
 
                 // Add the element to our container.
                 holder.appendChild(element);
             }
             container.appendChild(holder);
         });
+        document.body.appendChild(container);
     })
 
     // Put it all together
-    document.body.appendChild(container);
     console.log(`Initialized. Base RNG = ${baseRng}`);
 });
 
+console.log(baseRng);
 
 // Layout creation help
-function CreateElement(type, data, label = "") {
+function CreateElement(type, data, rngName, label = "") {
     switch (type) {
         case 'buttons':
-            return CreateButtons(data, label);
+            return CreateButtons(data, label, rngName);
             break;
         case 'radiobutton':
-            return CreateRadioButtons(data, label);
+            return CreateRadioButtons(data, label, rngName);
             break;
         case 'list':
-            return CreateList(data, label);
+            return CreateList(data, label, rngName);
             break;
     }
 }
 
 // "button" actually represents checkboxes.
-function CreateButtons(data, label) {
-    let list = document.createElement('div');
+function CreateButtons(data, label, rngName) {
+    let container = document.createElement('div');
 
     if (label.length > 0) {
         let title = document.createElement('span');
         title.innerHTML = label;
-        list.appendChild(title);
+        container.appendChild(title);
     }
 
     // Create the checkboxes
     data.forEach((item) => {
-        let option = document.createElement('div');
-        option.classList.add('form-check', 'form-check-inline');
+        let optGroup = document.createElement('div');
+        optGroup.classList.add('form-check', 'form-check-inline');
 
         // Create a unique id for label purposes
         let id = makeid(5);
@@ -85,30 +92,31 @@ function CreateButtons(data, label) {
         let box = document.createElement('input');
         box.classList.add('form-check-input');
         box.setAttribute("type", "checkbox");
+        box.setAttribute("ff8-rng", rngName);
         box.value = item.value;
         box.id = id;
-        option.appendChild(box);
+        optGroup.appendChild(box);
 
         // Label
         let elementLabel = document.createElement('label');
         elementLabel.classList.add('form-check-label');
         elementLabel.htmlFor = id;
         elementLabel.innerHTML = item.label;
-        option.appendChild(elementLabel);
+        optGroup.appendChild(elementLabel);
 
-        list.appendChild(option);
+        container.appendChild(optGroup);
     });
 
-    return list;
+    return container;
 }
 
-function CreateRadioButtons(data, label) {
-    let list = document.createElement('div');
+function CreateRadioButtons(data, label, rngName) {
+    let container = document.createElement('div');
 
     if (label.length > 0) {
         let title = document.createElement('span');
         title.innerHTML = label;
-        list.appendChild(title);
+        container.appendChild(title);
     }
 
     // Create a shared name so the radio buttons are associated with each other.
@@ -116,8 +124,8 @@ function CreateRadioButtons(data, label) {
 
     // Create the radio buttons
     data.forEach((item) => {
-        let option = document.createElement('div');
-        option.classList.add('form-check', 'form-check-inline');
+        let optGroup = document.createElement('div');
+        optGroup.classList.add('form-check', 'form-check-inline');
 
         // Create a unique id for label purposes
         let id = makeid(5);
@@ -126,27 +134,28 @@ function CreateRadioButtons(data, label) {
         let radio = document.createElement('input');
         radio.classList.add('form-check-input');
         radio.setAttribute("type", "radio");
+        radio.setAttribute("ff8-rng", rngName);
         radio.value = item.value;
         radio.id = id;
         radio.name = radioName;
-        option.appendChild(radio);
+        optGroup.appendChild(radio);
 
         // Label
         let elementLabel = document.createElement('label');
         elementLabel.classList.add('form-check-label');
         elementLabel.htmlFor = id;
         elementLabel.innerHTML = item.label;
-        option.appendChild(elementLabel);
+        optGroup.appendChild(elementLabel);
 
-        list.appendChild(option);
+        container.appendChild(optGroup);
     });
 
-    return list;
+    return container;
 }
 
-function CreateList(data, label) {
-    let list = document.createElement('div');
-    list.classList.add('col-auto');
+function CreateList(data, label, rngName) {
+    let container = document.createElement('div');
+    container.classList.add('col-auto');
 
     // Create a unique id
     let id = makeid(5);
@@ -154,40 +163,49 @@ function CreateList(data, label) {
     if (label.length > 0) {
         let title = document.createElement('span');
         title.innerHTML = label;
-        list.appendChild(title);
+        container.appendChild(title);
     }
 
     // Create the dropdown
     let select = document.createElement('select');
     select.id = id;
-    list.appendChild(select);
+    container.appendChild(select);
 
     // Add the dropdown options
     data.forEach((item) => {
         let option = document.createElement('option');
         option.value = item.value;
         option.text = item.label;
+        option.setAttribute("ff8-rng", rngName);
         select.appendChild(option);
     });
 
-    return list;
+    return container;
 }
 
 function CalculateRng() {
-    let rngTotal = 0;
-    // Get all values from select boxes
-    const listmatches = document.querySelectorAll("option:checked").forEach((option) => {
-        rngTotal += parseInt(option.value);
-    });
+    let div = document.getElementById('rng');
+    div.innerHTML = "";
+    // Iterate over each type of RNG we have
+    for (const propName in baseRng) {
+        let base = baseRng[propName];
+        let mod = 0;
 
-    // Get all values from checkboxes
-    const checkmatches = document.querySelectorAll("input:checked").forEach((option) => {
-        rngTotal += parseInt(option.value);
-    });
+        // Get all values from select boxes
+        const listmatches = document.querySelectorAll(`option[ff8-rng="${propName}"]:checked`).forEach((option) => {
+            mod += parseInt(option.value);
+        });
 
-    // Get all values from radio buttons
+        // Get all values from checkboxes & radio buttons
+        const checkmatches = document.querySelectorAll(`input[ff8-rng="${propName}"]:checked`).forEach((option) => {
+            mod += parseInt(option.value);
+        });
 
-    console.log(baseRng + rngTotal);
+        let calculatedRng = base + mod;
+        let p = document.createElement('p');
+        p.innerHTML = `${propName} RNG: ${calculatedRng}`;
+        div.appendChild(p);
+    }
 }
 
 // Helper functions
@@ -203,4 +221,7 @@ function makeid(length) {
     return result;
 }
 
-document.getElementById('go').addEventListener('click', CalculateRng);
+window.addEventListener('input', function (evt) {
+    CalculateRng();
+});
+CalculateRng();
